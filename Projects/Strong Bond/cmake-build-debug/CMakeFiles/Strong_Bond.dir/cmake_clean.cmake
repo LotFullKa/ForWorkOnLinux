79f7d94fd9e4020aabf6b82290e94cf8a40458b1
Kamil <lotfullin.kr@phystech.edu>
2020-02-17 18:55:11 +0300

    2 projects

---
+++ Projects/Strong Bond/cmake-build-debug/CMakeFiles/Strong_Bond.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Strong_Bond.dir/main.cpp.o"
+  "Strong_Bond"
+  "Strong_Bond.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Strong_Bond.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
