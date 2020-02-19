4e8e309ba6f6607c3422fcb287e1a51114e92937
Kamil <lotfullin.kr@phystech.edu>
2020-02-19 15:38:56 +0300

    uh blya

---
+++ Projects/Stong Bond/cmake-build-debug/CMakeFiles/Stong_Bond.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Stong_Bond.dir/main.cpp.o"
+  "Stong_Bond"
+  "Stong_Bond.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Stong_Bond.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
