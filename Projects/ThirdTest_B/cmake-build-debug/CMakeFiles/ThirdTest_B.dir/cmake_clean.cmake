2b576c0959591eed4c6226e65654c5d3a1dfe0ad
Kamil <lotfullin.kr@phystech.edu>
2020-05-08 13:09:07 +0300

    fast commit

---
+++ Projects/ThirdTest_B/cmake-build-debug/CMakeFiles/ThirdTest_B.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ThirdTest_B.dir/main.cpp.o"
+  "ThirdTest_B"
+  "ThirdTest_B.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ThirdTest_B.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
